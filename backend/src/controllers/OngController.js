ccc65056a412396a09dd6d88f098f8175498b2c8
Vinicius Monteiro <vmonteiro@gvdasa.com.br>
2020-10-18 23:40:47 -0300

    Adds routes validation and tests

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,10 +1,17 @@
 const crypto = require("crypto");
 const connection = require("../database/connection");
+const { index } = require("./ProfileController");
+const generateUniqueId = require("../utils/generateUniqueId");
 
 module.exports = {
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
+    return res.json(ongs);
+  },
+
   async create(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
-    const id = crypto.randomBytes(4).toString("hex");
+    const id = generateUniqueId();
     await connection("ongs").insert({
       id,
       name,
