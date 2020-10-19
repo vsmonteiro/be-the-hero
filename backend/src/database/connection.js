3ba00ea9cd66c6d68565b7a3c517e6a5a5637cbb
Vinicius Monteiro <vmonteiro@gvdasa.com.br>
2020-10-18 23:39:27 -0300

    Creates database for tests

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require("knex");
 const configs = require("../../knexfile");
 
-const connection = knex(configs.development);
+const connection = knex(
+  process.env.NODE_ENV !== "test" ? configs.development : configs.test
+);
 
 module.exports = connection;
